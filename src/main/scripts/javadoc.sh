c245482ae3ca386718c0551c018c92dab1e85170
Chris Mullins <chris.mullins10@gmail.com>
2013-04-06 16:41:17 -0700

    update javadoc

--- src/main/scripts/javadoc.sh
+++ src/main/scripts/javadoc.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 working_dir=$(mktemp -d -t javadoc.XXXXXXX)
 
 javadoc -d $working_dir -sourcepath src/main/java -subpackages org.sidoh -exclude ec.util:org.sidoh.wwf_api.types
-git checkout -b gh-pages origin/gh-pages
+git checkout gh-pages
 git pull 
 rm -rf javadoc
 mv $working_dir javadoc

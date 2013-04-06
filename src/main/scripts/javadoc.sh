62ae5ad0e087442c88e7a5d9ba42b564aa6aecc4
Chris Mullins <chris.mullins10@gmail.com>
2013-04-06 16:12:12 -0700

    update javadoc

--- src/main/scripts/javadoc.sh
+++ src/main/scripts/javadoc.sh
@@ -6,11 +6,11 @@ working_dir=$(mktemp -d -t javadoc.XXXXXXX)
 javadoc -d $working_dir -sourcepath src/main/java -subpackages org.sidoh -exclude ec.util:org.sidoh.wwf_api.types
 git checkout --orphan gh-pages
 git rm -rf *
-git pull
+git pull origin gh-pages
 echo '<a href="/wwf_api/javadoc">Javadoc</a>' > index.html
 git add index.html
 mv $working_dir javadoc
 git add javadoc
 git commit -am "update javadoc"
 git push origin gh-pages
-#git checkout master
+git checkout master

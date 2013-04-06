8506601e5c20cc0b40c791b570f46c98e73da45d
Chris Mullins <chris.mullins10@gmail.com>
2013-04-06 16:05:39 -0700

    javadoc gen script

--- src/main/scripts/javadoc.sh
+++ src/main/scripts/javadoc.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+working_dir=$(mktemp -d -t javadoc.XXXXXXX)
 
-javadoc -d $DIR/../../../gh-pages/javadoc -sourcepath src/main/java -subpackages org.sidoh -exclude ec.util:org.sidoh.wwf_api.types
+javadoc -d $working_dir -sourcepath src/main/java -subpackages org.sidoh -exclude ec.util:org.sidoh.wwf_api.types
+git checkout --orphan gh-pages
+git rm -rf *
+echo '<a href="/wwf_api/javadoc">Javadoc</a>' > index.html
+git add index.html
+mv $working_dir javadoc
+git add javadoc
+git commit -am "update javadoc"
+git push origin gh-pages
+git checkout master

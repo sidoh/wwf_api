783569627c60297f072fca3d67c5ba7d1f757626
Chris Mullins <chris.mullins10@gmail.com>
2013-04-09 20:19:39 -0700

    update javadoc generation script

--- src/main/scripts/javadoc.sh
+++ src/main/scripts/javadoc.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-working_dir=$(mktemp -d -t javadoc.XXXXXXX)
 
-javadoc -d $working_dir -sourcepath src/main/java -subpackages org.sidoh -exclude ec.util:org.sidoh.wwf_api.types
+cd $DIR/../../..
+
+tmphtml=$(mktemp -t index.html.XXXXX)
+echo "<!DOCTYPE html?>
+<html>
+  <head>
+    <title>wwf_api Javadoc</title>
+  </head>
+  <body>
+    <h1> wwf_api javadoc index </h1>
+    <hr/>
+    <ol>" > $tmphtml
+
+for tag in $(git tag); do
+  echo "<li><a href=\"javadoc/$tag\">$tag</a></li>" >> $tmphtml
+done
+
+echo "</ol></body></html>" >> $tmphtml
 git checkout gh-pages
-git pull 
-rm -rf javadoc
-mv $working_dir javadoc
-git add javadoc
-git commit -am "update javadoc"
-git push origin gh-pages:gh-pages
-git checkout master
-rm -rf javadoc
+git pull
+rm -rf javadoc/HEAD
+
+for tag in $(echo $(git tag) HEAD); do
+  if [ ! -e javadoc/$tag ]; then
+    git checkout $tag
+
+    working_dir=$(mktemp -d -t javadoc.XXXXXXX)
+    javadoc -d $working_dir -sourcepath src/main/java -subpackages org.sidoh -exclude ec.util:org.sidoh.wwf_api.types
+
+    git checkout gh-pages
+    mv $working_dir javadoc/$tag
+    git add javadoc
+    git commit -am "update javadoc for $tag"
+  fi
+done

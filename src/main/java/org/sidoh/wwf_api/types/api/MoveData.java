335fcdd7597fb175cb3f23b0166b9598070be0f5
dyap <dyap@rapleaf.com>
2013-04-20 19:46:08 -0700

    fix one tile case

--- src/main/java/org/sidoh/wwf_api/types/api/MoveData.java
+++ src/main/java/org/sidoh/wwf_api/types/api/MoveData.java
@@ -6,32 +6,19 @@
  */
 package org.sidoh.wwf_api.types.api;
 
+import org.apache.thrift.protocol.TTupleProtocol;
 import org.apache.thrift.scheme.IScheme;
 import org.apache.thrift.scheme.SchemeFactory;
 import org.apache.thrift.scheme.StandardScheme;
-
 import org.apache.thrift.scheme.TupleScheme;
-import org.apache.thrift.protocol.TTupleProtocol;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.EnumMap;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.EnumSet;
-import java.util.Collections;
-import java.util.BitSet;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.util.*;
 
 /**
  * Encapsulates metadata about a single move. This will represent moves that are
  * included as part of the game state.
  */
-public class MoveData implements org.apache.thrift.TBase<MoveData, MoveData._Fields>, java.io.Serializable, Cloneable {
+public class  MoveData implements org.apache.thrift.TBase<MoveData, MoveData._Fields>, java.io.Serializable, Cloneable {
   private static final org.apache.thrift.protocol.TStruct STRUCT_DESC = new org.apache.thrift.protocol.TStruct("MoveData");
 
   private static final org.apache.thrift.protocol.TField BOARD_CHECKSUM_FIELD_DESC = new org.apache.thrift.protocol.TField("boardChecksum", org.apache.thrift.protocol.TType.I32, (short)1);

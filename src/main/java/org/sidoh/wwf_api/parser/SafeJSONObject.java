31bc57db5ba3b83ea759cbc418514b02fd038a77
Chris Mullins <chris.mullins10@gmail.com>
2013-04-25 22:33:58 -0700

    remove extraneous overrides

--- src/main/java/org/sidoh/wwf_api/parser/SafeJSONObject.java
+++ src/main/java/org/sidoh/wwf_api/parser/SafeJSONObject.java
@@ -1,11 +1,9 @@
 package org.sidoh.wwf_api.parser;
 
-import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import net.minidev.json.JSONStyle;
 
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * Wraps standard JSONObject and throws exceptions if requested keys are not present
@@ -15,30 +13,6 @@ public class SafeJSONObject extends JSONObject {
 
   public SafeJSONObject(JSONObject wrapped) {
     this.wrapped = wrapped;
-  }
-
-  public static String escape(String s) {
-    return JSONObject.escape(s);
-  }
-
-  public static String toJSONString(Map<String, ? extends Object> map) {
-    return JSONObject.toJSONString(map);
-  }
-
-  public static String toJSONString(Map<String, ? extends Object> map, JSONStyle compression) {
-    return JSONObject.toJSONString(map, compression);
-  }
-
-  public static void writeJSONKV(String key, Object value, Appendable out, JSONStyle compression) throws IOException {
-    JSONObject.writeJSONKV(key, value, out, compression);
-  }
-
-  public static void writeJSON(Map<String, Object> map, Appendable out) throws IOException {
-    JSONObject.writeJSON(map, out);
-  }
-
-  public static void writeJSON(Map<String, ? extends Object> map, Appendable out, JSONStyle compression) throws IOException {
-    JSONObject.writeJSON(map, out, compression);
   }
 
   @Override
@@ -54,14 +28,6 @@ public class SafeJSONObject extends JSONObject {
   @Override
   public void merge(Object o2) {
     wrapped.merge(o2);
-  }
-
-  public static JSONObject merge(JSONObject o1, Object o2) {
-    return JSONObject.merge(o1, o2);
-  }
-
-  public static JSONArray merge(JSONArray o1, Object o2) {
-    return JSONObject.merge(o1, o2);
   }
 
   @Override
